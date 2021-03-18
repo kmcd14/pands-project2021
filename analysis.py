a809e4187fde1a5749c0fd435e7e837c32d5e906
Katie Mc D <katieisanimdom@gmail.com>
2021-03-18 20:49:03 +0000

    added iris.csv file. Created analysis.py and imported numpy, panadaa, matplotlib and seaborn

---
+++ analysis.py
@@ -0,0 +1,19 @@
+# This program is a data analysis on the Iris dataset for programming & scripting module
+# Author: Katie Mc Donald
+
+# Importing libaries
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#from sklearn.datasets import load_iris
+#iris = load_iris()
+#print(iris.DESCR)
+#def load_data():
+   # iris = pd.read_csv('iris.cvs')
+    #iris.columns = ['Sepal Lenght', 'Sepal Width', 'Petal Lenght', 'Petal Width', 'Species']
+    #return iris
+
+
+print(load_data)
