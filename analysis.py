8a222cb125f46994229abbf541c2683ee7b96b91
Katie Mc D <katieisanimdom@gmail.com>
2021-03-18 21:17:24 +0000

    read in the iris.csv file into a pandas dataframe and added column titles to the data

--- analysis.py
+++ analysis.py
@@ -12,15 +12,3 @@ df = pd.read_csv('iris.csv')
 # Adding column titles to the data
 df.columns = ['Sepal Lenght', 'Sepal Width', 'Petal Lenght', 'Petal Width', 'Species']
 print(df)
-
-
-#from sklearn.datasets import load_iris
-#iris = load_iris()
-#print(iris.DESCR)
-#def load_data():
-   # iris = pd.read_csv('iris.cvs')
-    #iris.columns = ['Sepal Lenght', 'Sepal Width', 'Petal Lenght', 'Petal Width', 'Species']
-    #return iris
-
-
-#print(load_data)
